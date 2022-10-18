d5906adca6ca81e2cf55606a17efde2bfdd190bb
zwc819konkuk <104195503+zwc819konkuk@users.noreply.github.com>
2022-10-18 22:11:32 +0900

    spring aop注解02 5种通知

--- spring_aop_01/src/main/java/com/zwc/aspect/DaoAspect.java
+++ spring_aop_01/src/main/java/com/zwc/aspect/DaoAspect.java
@@ -1,14 +1,71 @@
 package com.zwc.aspect;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
 public class DaoAspect {
-    @Before("execution(* com.zwc.dao.impl.UserDaoImpl.addUser(..))")
-    public void methodBefore(){
+    //定义一个公共切点 切点表达式直接指向接口 降低耦合度
+    @Pointcut("execution(* com.zwc.dao.*.add*(..))")
+    public void addPointCut() {
+    }
+
+    @Before("addPointCut()")
+    public void methodBefore(JoinPoint joinPoint) {
         System.out.println("method before invoked...");
+        Object[] args = joinPoint.getArgs();
+        System.out.println("args:" + Arrays.toString(args));
+    }
+
+    /**
+     * 无论切点是否出现异常 都会执行
+     * 后置也叫最终通知
+     */
+    @After("addPointCut()")
+    public void methodAfter() {
+        System.out.println("methodAfter invoked...");
+    }
+
+    /**
+     * 返回通知：在切点方法返回结果之后增强的功能
+     * 切点方法如果出现异常 则不执行
+     *
+     * @param joinPoint 切点
+     * @param res       切点方法返回值
+     */
+    @AfterReturning(value = "addPointCut()", returning = "res")
+    public void methodAfterReturning(JoinPoint joinPoint, Object res) {
+        System.out.println("methodAfterReturning invoked...");
+        System.out.println(res);
+    }
+
+    /**
+     * 异常通知：切点方法出现异常时执行
+     * 可以接受异常对象
+     */
+    @AfterThrowing(value = "addPointCut()", throwing = "ex")
+    public void methodAfterThrowing(Exception ex) {
+        System.out.println("methodAfterThrowing invoked...");
+        System.out.println(ex.getMessage());
+    }
+
+
+    /**
+     * 环绕通知：在切点方法之前和之后执行
+     * 环绕通知的返回值必须是object 将切点方法继续向上返回
+     *
+     * @param proceedingJoinPoint 切点,手动控制切点方法执行的位置
+     */
+    @Around("addPointCut()")
+    public Object methodAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        System.out.println("methodAroundA invoked...");
+        Object res = proceedingJoinPoint.proceed();
+        System.out.println("methodAroundB invoked...");
+        return res;
     }
 }
--- spring_aop_01/src/test/java/com/zwc/Test02.java
+++ spring_aop_01/src/test/java/com/zwc/Test02.java
@@ -1,5 +1,6 @@
 package com.zwc;
 
+import com.zwc.dao.EmpDao;
 import com.zwc.dao.UserDao;
 import com.zwc.service.UserService;
 import org.junit.Test;
@@ -9,9 +10,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test02 {
 
     @Test
-    public void testBean(){
+    public void testBean() {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean(UserDao.class);
-        dao.addUser(1,"jack");
+        int rows = dao.addUser(1, "jack");
+        System.out.println(rows);
+
+        System.out.println("==========================");
+
+        EmpDao empDao = context.getBean(EmpDao.class);
+        int rows1 = empDao.addEmp(1, "jack", "ceo");
+        System.out.println(rows1);
     }
 }
