e0b15edf5102a4e31c20701d6c4ece924c367429
zwc819konkuk <104195503+zwc819konkuk@users.noreply.github.com>
2022-11-09 17:24:28 +0900

    ssm配置完成

--- ssm01/src/main/java/com/zwc/service/UserService.java
+++ ssm01/src/main/java/com/zwc/service/UserService.java
@@ -1,4 +1,12 @@
 package com.zwc.service;
 
 public interface UserService {
+    /*
+    * SQLSession.getMapper()
+    *
+    * 在service层注入Mapper对象【MyBatis生成】
+    *
+    * sqlSessionFactory SqlSession **Mapper 全部交给Spring创建
+    * 在Spring容器中存储**Mapper，再向service注入
+    * */
 }
--- ssm01/src/main/java/com/zwc/service/impl/UserServcieImpl.java
+++ ssm01/src/main/java/com/zwc/service/impl/UserServcieImpl.java
@@ -1,6 +1,10 @@
 package com.zwc.service.impl;
 
+import com.zwc.mapper.UserMapper;
 import com.zwc.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserServcieImpl implements UserService {
+    @Autowired
+    private UserMapper userMapper;
 }
